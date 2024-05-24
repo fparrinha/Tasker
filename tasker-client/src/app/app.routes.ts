16ba93ee2f6785e37545163b0c5b94f2110b17e4
fparrinha <francisco.parrinha15@gmail.com>
2024-05-24 15:21:12 +0200

    Added error page

--- tasker-client/src/app/app.routes.ts
+++ tasker-client/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { TaskerComponent } from './tasker/components/tasker/tasker.component';
 import { LoginComponent } from './users/components/login/login.component';
 import { RegisterComponent } from './users/components/register/register.component';
+import { ErrorPageComponent } from './core/components/error-page/error-page.component';
 
 export const routes: Routes = [
-    {path: "", redirectTo: "/login", pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
-    {path: 'tasker', component: TaskerComponent}];
+    {path: 'tasker', component: TaskerComponent},
+    {path: "", redirectTo: "/login", pathMatch: 'full'},
+    {path: "**", component: ErrorPageComponent, pathMatch: 'full'}];
